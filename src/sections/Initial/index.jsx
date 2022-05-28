a3c81bd4eaeb025696ef2aa376e48858047dd4f4
Bolio <again.bolio@hotmail.com>
2022-05-28 15:06:49 -0500

    Add Initial section

---
+++ src/sections/Initial/index.jsx
@@ -0,0 +1,74 @@
+import styled from "styled-components";
+import Text from "../../components/Texts";
+import ButtonText from "../../components/Buttons";
+
+const InitialSectionStyled = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 100%;
+  padding: 30px 100px 0 100px;
+`;
+
+const InfoContactStyled = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr;
+  div {
+    margin-top: 100px;
+    p {
+      margin: 0;
+    }
+    p:nth-child(1) {
+      padding: 20px 101px 20px 0;
+    }
+    p:nth-child(2) {
+      padding: 20px 160px 20px 0;
+    }
+    button {
+      margin-top: 20px;
+    }
+  }
+`;
+
+const ImgVideoStyled = styled.div`
+  width: 100%;
+  height: fit-content;
+  text-align: center;
+  img {
+    /* height: 100%; */
+    object-fit: cover;
+    border-radius: 300px;
+    /* margin: 0 auto; */
+  }
+`;
+
+const InitialSection = () => {
+  return (
+    <>
+      {/* ROJO - 2 columns */}
+      <InitialSectionStyled>
+        <InfoContactStyled>
+          <div>
+            <Text fontSize="60px" fontWeight="bolder">
+              Lorem ipsum dolor sit amet, consectetur adipiscing.
+            </Text>
+            <Text fontSize="16px">
+              Aliquam faucibus purus in massa tempor nec feugiat nisl pretium.
+              Lectus quam id leo in. Aliquam sem et tortor consequat id porta
+              nibh venenatis. Dui nunc mattis enim ut tellus elementum. Leo
+              integer malesuada nunc vel.
+            </Text>
+            <ButtonText width="160px" height="40px" borderRadius="40px">
+              CONTACTO
+            </ButtonText>
+          </div>
+        </InfoContactStyled>
+        <ImgVideoStyled>
+          <img src="src/assets/img-video.jpg" alt="img-video" />
+        </ImgVideoStyled>
+      </InitialSectionStyled>
+    </>
+  );
+};
+
+export default InitialSection;
