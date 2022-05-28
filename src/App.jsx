bc323c50c6d5a62a82cd2fd4f2a5fa295aaf7983
Bolio <again.bolio@hotmail.com>
2022-05-28 15:09:27 -0500

    Add components to use and theme of styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,23 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import "./App.css";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styled/themes/theme";
+import Layout from "./components/Layout";
+import Title from "./components/Title";
+import InitialSection from "./sections/Initial";
+import CarouselSection from "./sections/Carousel";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    // <div className="App">
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Title imgSrc="src/assets/Logo.png" altInfo="Logo-Title" />
+        <InitialSection />
+        <CarouselSection />
+      </Layout>
+    </ThemeProvider>
+    // </div>
+  );
 }
 
-export default App
+export default App;
