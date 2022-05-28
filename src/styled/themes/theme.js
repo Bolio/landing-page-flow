67293ea29527acda91ad0303b44a944271092dc3
Bolio <again.bolio@hotmail.com>
2022-05-28 15:05:00 -0500

    Add theme with styles

---
+++ src/styled/themes/theme.js
@@ -0,0 +1,20 @@
+export const theme = {
+  colors: {
+    blue: "#1353D4",
+    lightBlue: "#4DFFF3",
+    purple: "#331E38",
+    darkPurple: "#180E1B",
+  },
+  fontWeight: {
+    light: 500,
+    medium: 700,
+    bolder: 900,
+  },
+  fontSize: {
+    xSmall: "0.875rem",
+    small: "1rem",
+    medium: "1.125rem",
+    large: "1.5rem",
+    xLarge: "1.875rem",
+  },
+};
