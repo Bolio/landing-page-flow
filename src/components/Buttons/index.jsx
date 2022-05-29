a88ead5f84cb3ce710993667cca021ac90eaf80a
Bolio <again.bolio@hotmail.com>
2022-05-29 16:05:30 -0500

    Changes in Button component

--- src/components/Buttons/index.jsx
+++ src/components/Buttons/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const ButtonTextStyled = styled.button`
   color: ${(props) => props.theme.colors.darkPurple};
-  background-color: ${(props) => props.theme.colors.lightBlue};
+  /* background-color: ${(props) => props.theme.colors.lightBlue}; */
   width: ${({ width }) => width};
   height: ${({ height }) => height};
   font-size: ${(props) => props.theme.fontSize.small};
@@ -17,11 +17,28 @@ const ButtonTextStyled = styled.button`
     border: 1px inset #1d659f;
     padding: 4px 2px 2px 4px;
   }
+
+  ${(props) =>
+    props.status === "primary" &&
+    css`
+      background-color: ${(props) => props.theme.colors.lightBlue};
+    `}
+  ${(props) =>
+    props.status === "secondary" &&
+    css`
+      background-color: white;
+      border: 2px ${(props) => props.theme.colors.lightBlue} solid;
+    `}
 `;
 
-const ButtonText = ({ children, width, height, borderRadius }) => {
+const ButtonText = ({ children, width, height, borderRadius, status }) => {
   return (
-    <ButtonTextStyled width={width} height={height} borderRadius={borderRadius}>
+    <ButtonTextStyled
+      width={width}
+      height={height}
+      borderRadius={borderRadius}
+      status={status}
+    >
       {children}
     </ButtonTextStyled>
   );
