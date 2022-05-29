a1118074168160a94f827994b710f0a6b1608925
Bolio <again.bolio@hotmail.com>
2022-05-29 16:05:48 -0500

    Changes in Text component

--- src/components/Texts/index.jsx
+++ src/components/Texts/index.jsx
@@ -3,15 +3,28 @@ import styled from "styled-components";
 
 const TextStyled = styled.p`
   /* color: green; */
-  margin: 20px 0;
+  /* margin: 20px 0; */
   font-size: ${({ fontSize }) => fontSize};
   font-weight: ${({ fontWeight }) => fontWeight};
   text-align: ${({ textAlign }) => textAlign};
+  color: ${(props) => props.color === "lightBlue" ? props.theme.colors.lightBlue : null};
+
+  /* ${(props) =>
+    props.status === "title" &&
+    css`
+      background-color: white;
+      border: 2px ${(props) => props.theme.colors.lightBlue} solid;
+    `} */
 `;
 
-const Text = ({ children, fontSize, fontWeight, textAlign }) => {
+const Text = ({ children, fontSize, fontWeight, textAlign, color }) => {
   return (
-    <TextStyled fontSize={fontSize} fontWeight={fontWeight} textAlign={textAlign}>
+    <TextStyled
+      fontSize={fontSize}
+      fontWeight={fontWeight}
+      textAlign={textAlign}
+      color={color}
+    >
       {children}
     </TextStyled>
   );
